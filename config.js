6d8606d710b33218219815479ad70a4e30cb46bb
Anhuynh2404 <huynhcongan2442004@gmail.com>
2024-08-17 18:56:46 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Em à!',
+    introDesc: `Trái đất vốn lạ thưỞng
+    Mà sao em cứ đi nhầm đưỞng
+    Lạc vào tim anh lẻ loi
+    Ğằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'My Idol ^_^',
+   // desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cưỞi ấy ',
+    btnYes: 'Phải thử thoiii',
+    btnNo: 'Thôi sợ quá😢',
+    question:'Trên thế giới hơn 7 tỉ ngưỞi mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Quyết định đúng đắn ròi đó e. Nói iiii',
+    //messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii',
+    messLink: 'https://www.facebook.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
